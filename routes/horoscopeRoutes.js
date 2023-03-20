c623f3eeb828caea0e6753806f4b5faac4794a69
Satyam Dahlan <dahlansatyam@gmail.com>
2023-03-21 01:06:33 +0530

    test

--- routes/horoscopeRoutes.js
+++ routes/horoscopeRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const { horoscope } = require("../controllers/horoscopeController");
 
 router.get("/:zodiacName", horoscope);
+router.get("/", (req, res) => {
+  res.json({ msg: "working" });
+});
 
 module.exports = router;
